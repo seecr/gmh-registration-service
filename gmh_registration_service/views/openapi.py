52834605f17b7af1af0f14437301ef70e87c7553
Johan Jonkers <johan@seecr.nl>
2025-06-04 09:17:55 +0200

    JJ: editor in CORS

--- gmh_registration_service/views/openapi.py
+++ gmh_registration_service/views/openapi.py
@@ -7,4 +7,8 @@ from .._path import static_path
 # Test at: https://editor.swagger.io/
 #
 async def openapi(request, **_):
-    return FileResponse(static_path / "openapi.yaml", media_type="text/yaml")
+    return FileResponse(
+        static_path / "openapi.yaml",
+        media_type="text/yaml",
+        headers={"Access-Control-Allow-Origin": "https://editor.swagger.io"},
+    )
