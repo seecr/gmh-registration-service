6c1089f888687191f432ac49495593293f29ecc8
Thijs Janssen <thijs@seecr.nl>
2025-06-10 11:40:38 +0200

    TJ/JJ: license

--- gmh_registration_service/views/token_test.py
+++ gmh_registration_service/views/token_test.py
@@ -1,3 +1,29 @@
+## begin license ##
+#
+# Gemeenschappelijke Metadata Harvester (GMH) Registration Service
+#  register NBN and urls
+#
+# Copyright (C) 2025 Koninklijke Bibliotheek (KB) https://www.kb.nl
+# Copyright (C) 2025 Seecr (Seek You Too B.V.) https://seecr.nl
+#
+# This file is part of "GMH-Registration-Service"
+#
+# "GMH-Registration-Service" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "GMH-Registration-Service" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "GMH-Registration-Service"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
 from gmh_registration_service.test_utils import (
     environment,
     environment_session,
