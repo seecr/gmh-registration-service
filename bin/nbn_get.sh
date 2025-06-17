95448adc906064070f25bf6c82ded6efb4b85441
Johan Jonkers <johan@seecr.nl>
2025-06-17 14:00:23 +0200

    JJ: Aanpassingen

--- bin/nbn_get.sh
+++ bin/nbn_get.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
+## begin license ##
+#
+# Gemeenschappelijke Metadata Harvester (GMH) Registration Service
+#  register NBN and urls
+#
+# Copyright (C) 2025 Koninklijke Bibliotheek (KB) https://www.kb.nl
+# Copyright (C) 2025 Seecr (Seek You Too B.V.) https://seecr.nl
+#
+# This file is part of "GMH-Registration-Service"
+#
+# "GMH-Registration-Service" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "GMH-Registration-Service" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "GMH-Registration-Service"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
 #
 
+if [ -z "${RESOLVER_URL}" ]; then
+    echo "Please set RESOLVER_URL to something like https://resolver-ui.acc.kb.seecr.nl/gmh-registration-service"
+    exit 1
+fi
 
 if [ -z "${TOKEN}" ]; then
     TOKEN=$1; shift 1;
@@ -9,7 +39,7 @@ fi
 NBN=$1
 
 RESPONSE=$(wget --quiet \
-    -O- "https://registration-service.kb.dev.seecr.nl/nbn/${NBN}" \
+    -O- "${RESOLVER_URL}/nbn/${NBN}" \
     --header "Authorization: Bearer ${TOKEN}")
 
 echo "RESPONSE: ${RESPONSE}"
